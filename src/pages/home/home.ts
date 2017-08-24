f10345a91af0e0e75bf43b5f2d49d782b25b3933
Marcelo <hideaki09@hotmail.com>
2017-08-23 21:02:48 -0300

    Sobre atualizado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 @Component({
   selector: 'page-home',
