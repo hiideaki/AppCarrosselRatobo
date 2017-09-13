0cb76a60fb534cf1bb663b3bbdeb515837510767
Marcelo <hideaki09@hotmail.com>
2017-09-13 00:24:42 -0300

    Variáveis relacionadas e bluetooth pronto para ser configurado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,22 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private speed: number;
+  private ledc: number[];
+  private macAddress: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public bluetoothSerial: BluetoothSerial) {
+    this.speed = 0;
+    this.ledc = [3, 3, 3];
+    this.macAddress = "00:00:00:00:00:00";
+    this.bluetoothSerial.enable();
+  }
+
+  public sendByte() {
+    var byte = new Uint8Array(1);
 
   }
+
+
 
 }
