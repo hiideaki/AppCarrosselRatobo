ba6735c3ecffd1e972103c0e9d26377ce1702ead
Marcelo <hideaki09@hotmail.com>
2017-09-20 13:25:57 -0300

    Teste de envio de bits (LED)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,19 +10,33 @@ export class HomePage {
   private speed: number;
   private ledc: number[];
   private macAddress: string;
+  private byte;
 
   constructor(public navCtrl: NavController, public bluetoothSerial: BluetoothSerial) {
     this.speed = 0;
     this.ledc = [3, 3, 3];
     this.macAddress = "00:00:00:00:00:00";
+    this.byte = new Uint8Array(1);
     this.bluetoothSerial.enable();
   }
 
   public sendByte() {
-    var byte = new Uint8Array(1);
-
+    this.bluetoothSerial.write(this.byte).then((sucess)=>{},
+      (err)=>{});
   }
 
+  public mudaVelocidade() {
+    console.log("VELOCIDADE = " +this.speed);
+  }
+
+  //Altera e envia a cor do LED
+  public mudaLed(i: number) {
+    console.log("LED[" + i + "] = " + this.ledc[i]);
+    //(10) + (R) + (G) + (B)
+    this.byte[0] = 2 * 64 + this.ledc[0] * 16 + this.ledc[1] * 4 + this.ledc[2];
+    console.log(this.byte[0]);
+    this.sendByte();
+  }
 
 
 }
