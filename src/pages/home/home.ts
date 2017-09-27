9eef413385f22f83b5c963d509e4b071a4b7fd29
Marcelo <hideaki09@hotmail.com>
2017-09-26 23:49:22 -0300

    Montagem dos bytes de movimento e LED prontos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,11 +11,13 @@ export class HomePage {
   private ledc: number[];
   private macAddress: string;
   private byte;
+  private dir: number[];
 
   constructor(public navCtrl: NavController, public bluetoothSerial: BluetoothSerial) {
     this.speed = 0;
     this.ledc = [3, 3, 3];
-    this.macAddress = "00:00:00:00:00:00";
+    this.dir = [1, 1];
+    this.macAddress = "00:21:13:00:4F:0D";
     this.byte = new Uint8Array(1);
     this.bluetoothSerial.enable();
   }
@@ -25,22 +27,32 @@ export class HomePage {
       (err)=>{});
   }
 
-  public mudaVelocidade() {
-    console.log("VELOCIDADE = " +this.speed);
-  }
-
-  //Altera e envia a cor do LED
-  public mudaLed(i: number) {
-    console.log("LED[" + i + "] = " + this.ledc[i]);
-    //(10) + (R) + (G) + (B)
-    this.byte[0] = 2 * 64 + this.ledc[0] * 16 + this.ledc[1] * 4 + this.ledc[2];
-    console.log(this.byte[0]);
+  public mudaVelocidade(i: number) {
+    if(i == 1) {
+      this.dir[0] = 0;
+      this.dir[1] = 1;
+    } else if(i == 2) {
+      this.dir[0] = 1;
+      this.dir[1] = 0;
+    } else if(i == 3) {
+      this.dir[0] = 1;
+      this.dir[1] = 1;
+    }
+    this.byte[0] = 2 * 64 + (1 + this.speed * this.dir[0]) * 8 + (1 + this.speed * this.dir[1]) * 1;
     this.sendByte();
   }
 
-  public teste(i: number) {
-    console.log("FOI " + i);
+  public connectModule(){
+    this.bluetoothSerial.list().then((con)=>{
+      this.bluetoothSerial.connect(this.macAddress).subscribe();
+    });
   }
 
 
+  //Altera e envia a cor do LED
+  public mudaLed(i: number) {
+    //(10) + (R) + (G) + (B)
+    this.byte[0] = 1 * 64 + this.ledc[0] * 16 + this.ledc[1] * 4 + this.ledc[2];
+    this.sendByte();
+  }
 }
