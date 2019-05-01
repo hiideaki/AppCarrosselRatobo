5949ab25b5e02c8a94dd8fa33cee3f67c6e6d870
hiideaki <hideaki09@hotmail.com>
2019-05-01 12:34:28 -0300

    App finalizado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,7 +11,6 @@ import * as nipplejs from 'nipplejs';
 
 export class HomePage {
     private speed: number;
-    private ledc: number[];
     private macAddress: string;
     private dispCon;
     private byte;
@@ -19,47 +18,50 @@ export class HomePage {
     private drive;
     private leftM;
     private rightM;
+    private joystickSize;
 
     constructor(public navCtrl: NavController, public bluetoothSerial: BluetoothSerial, private alertCtrl: AlertController) {
         this.speed = 0;
-        this.ledc = [3, 3, 3];
         this.macAddress = "";
-        this.byte = new Uint8Array(18);
+        this.byte = new Uint8Array(1);
         this.bluetoothSerial.enable();
+        this.joystickSize = 300;
     }
 
 /*  Ao terminar de carregar a página cria o joystick e o associa com suas devidas funções   */
 /*  A variável 'options' se refere aos parâmetros para a criação do joystic, que no caso será estático  */
     ionViewDidLoad() {
         var parent = this;
-        var options = {
-            zone: document.querySelector('.zone.static'),
-            mode: 'static',
-            position: {
-                left: '50%',
-                top: '50%'
-            },
-            color: 'blue',
-            size: 200
-        };
 /*  Dá um delay na criação do joystick de fato para posicioná-lo corretamente na interface  */
         setTimeout(function() {
+            var options = {
+                zone: document.querySelector('.zone.static'),
+                mode: 'static',
+                position: {
+                    left: '50%',
+                    top: '50%'
+                },
+                color: 'blue',
+                size: parent.joystickSize
+            };
             parent.joystick = nipplejs.create(options);
             parent.bindNipple();    
         }, 500);
+
     }
+    
 
 /*  Funções associadas ao joystick  */
     public bindNipple() {
         var parent = this;
         this.joystick.on('end', function(evt, data) {
-            parent.textAppend('Tentando parar motor!');
 /*  Ao soltá-lo chama a função paraMotor()  */
             parent.paraMotor(); 
-        }).on('move', function(evt, data) {             
-            parent.textAppend('Mexendo!');
+        }).on('move', function(evt, data) {          
 /*  Quando é movimentado, chama a função montaByte(), passando como parâmetros o ângulo e a distância do centro */
-            parent.montaByte(data.angle.degree, data.distance);
+            if(data.distance > 30) {
+                parent.montaByte(data.angle.degree, data.distance);
+            }
         });
     }
 
@@ -108,7 +110,8 @@ export class HomePage {
 7 - nothing
 */
     public montaByte(angle, speed) {
-        this.drive = Math.ceil(speed * 2 / 100);
+
+        this.drive = Math.ceil(speed * 4 / this.joystickSize);
         switch(true) {
             case angle > 22.5 && angle <= 67.5:
                 this.leftM = (1 + this.drive) * 2;
@@ -147,23 +150,19 @@ export class HomePage {
         this.sendByte();
     }
     
-/*  Função com fins de depuração apenas. Adiciona ao console o texto passado como parâmetro */
-    public textAppend(texto) {
-        var txt = document.createTextNode(texto + '\n')
-        document.getElementById('console').appendChild(txt);
-        document.getElementById('console').scrollTop = 999999;
-    }
 
 /*  Função com fins de depuração apenas. Converte um número decimal para seu formato binário equivalente    */
-    public dec2bin(dec){
+    public dec2bin(dec) {
         return (dec >>> 0).toString(2);
     }
 
 /*  Envia o byte ao dispositivo conectado pelo bluetooth    */
     public sendByte() {
-        let c: string = String.fromCharCode(this.byte[0]);
-        this.bluetoothSerial.write(c).then((success)=>{this.textAppend(this.byte[0])},
-           (err)=>{this.textAppend('Deu ruim')});
+        // let c: string = String.fromCharCode(this.byte[0]);
+        // console.log(this.dec2bin(this.byte[0]))
+        console.log(this.byte)
+        this.bluetoothSerial.write(this.byte[0]).then((success)=>{},
+        (err)=>{});
     }
     
 /*  Envia um byte comandando a parada do motor  */
@@ -171,14 +170,4 @@ export class HomePage {
         this.byte[0] = 0 * 64 + 2 * 8 + 2 * 1; // 00 010 010
         this.sendByte();
     }
-
-/*  Não foi implementado no Arduino */
-/*  Envia um byte comandando a mudança de cores do LED  */
-/*
-    public mudaLed(i: number) {
-        //(10) + (R) + (G) + (B)
-        this.byte[0] = 1 * 64 + this.ledc[0] * 16 + this.ledc[1] * 4 + this.ledc[2];
-        this.sendByte();
-    }
-*/
 }
